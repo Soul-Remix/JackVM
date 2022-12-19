a5ebca3ff1c0c0b68bf1ff6d28efbf83c5c0b1a6
Soul-Remix <soulremix-dev@pm.me>
2022-12-19 17:44:58 +0300

    Add push that, static, constant commands

--- VM-Translator/src/main/java/Translator/PopPushWriter.java
+++ VM-Translator/src/main/java/Translator/PopPushWriter.java
@@ -4,9 +4,11 @@ import java.io.PrintWriter;
 
 class PopPushWriter {
     private final PrintWriter writer;
+    private final String name;
 
-    public PopPushWriter(PrintWriter writer) {
+    public PopPushWriter(String name, PrintWriter writer) {
         this.writer = writer;
+        this.name = name;
     }
 
     public void translatePushArgument(int index) {
@@ -29,7 +31,7 @@ class PopPushWriter {
         writer.println("M=M+1");
     }
 
-    private void translatePushLocal(int index) {
+    public void translatePushLocal(int index) {
         writer.println("// push local " + index);
 
         // Load (segment + index) content
@@ -49,7 +51,7 @@ class PopPushWriter {
         writer.println("M=M+1");
     }
 
-    private void translatePushThis(int index) {
+    public void translatePushThis(int index) {
         writer.println("// push this " + index);
 
         // Load (segment + index) content
@@ -58,6 +60,60 @@ class PopPushWriter {
         writer.println("@" + index);
         writer.println("A=D+A");
         writer.println("D=M");
+
+        // Push to stack
+        writer.println("@SP");
+        writer.println("A=M");
+        writer.println("M=D");
+
+        // Update stack pointer
+        writer.println("@SP");
+        writer.println("M=M+1");
+    }
+
+    public void translatePushThat(int index) {
+        writer.println("// push that " + index);
+
+        // Load (segment + index) content
+        writer.println("@THAT");
+        writer.println("D=M");
+        writer.println("@" + index);
+        writer.println("A=D+A");
+        writer.println("D=M");
+
+        // Push to stack
+        writer.println("@SP");
+        writer.println("A=M");
+        writer.println("M=D");
+
+        // Update stack pointer
+        writer.println("@SP");
+        writer.println("M=M+1");
+    }
+
+    public void translatePushStatic(int index) {
+        writer.println("// push static " + index);
+
+        // Load (segment + index) content
+        writer.println("@" + name + ".static." + index);
+        writer.println("D=M");
+
+        // Push to stack
+        writer.println("@SP");
+        writer.println("A=M");
+        writer.println("M=D");
+
+        // Update stack pointer
+        writer.println("@SP");
+        writer.println("M=M+1");
+    }
+
+    public void translatePushConstant(int index) {
+        writer.println("// push constant " + index);
+
+        // Load (segment + index) content
+        writer.println("@" + index);
+        writer.println("D=A");
 
         // Push to stack
         writer.println("@SP");
