bf9acf48e59228b732301acbd5fcdcb7420ce955
Soul-Remix <soulremix-dev@pm.me>
2022-12-19 17:13:44 +0300

    Complete neg, eq, gt commands in CodeWriter

--- VM-Translator/src/main/java/Translator/CodeWriter.java
+++ VM-Translator/src/main/java/Translator/CodeWriter.java
@@ -5,10 +5,16 @@ import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 
 public class CodeWriter {
-    private PrintWriter writer;
+    private final PrintWriter writer;
+    private final String outputName;
+    private int eqCount;
+    private int gtCount;
 
     public CodeWriter(String output) throws IOException {
-        PrintWriter writer = new PrintWriter(output + ".vm", StandardCharsets.UTF_8);
+        writer = new PrintWriter(output + ".vm", StandardCharsets.UTF_8);
+        outputName = output;
+        eqCount = 0;
+        gtCount = 0;
     }
 
     public void writeArithmetic(String command) {
@@ -17,6 +23,15 @@ public class CodeWriter {
         }
         if(command.equals("sub")) {
             translateCommandSub();
+        }
+        if(command.equals("neg")) {
+            translateCommandNeg();
+        }
+        if(command.equals("eq")) {
+            translateCommandEQ();
+        }
+        if(command.equals("gt")) {
+            translateCommandGT();
         }
     }
 
@@ -84,5 +99,109 @@ public class CodeWriter {
         writer.println("@SP");
         writer.println("A=M-1");
         writer.println("M=D");
+    }
+
+    private void translateCommandNeg() {
+        writer.println("// neg");
+
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("M=-M");
+    }
+
+    private void translateCommandEQ() {
+        String labelEQTrue = outputName + ".EQ.true." + eqCount;
+        String labelEQEnd = outputName + ".EQ.end." + eqCount;
+        eqCount++;
+
+        writer.println("// eq");
+
+        // Load second operand
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("D=M");
+        writer.println("@R13");
+        writer.println("M=D");
+
+        // Update stack pointer
+        writer.println("@SP");
+        writer.println("M=M-1");
+
+        // Load first operand
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("D=M");
+
+        // Sub
+        writer.println("@R13");
+        writer.println("D=D-M");
+
+        // Check to jump
+        writer.println("@" + labelEQTrue);
+        writer.println("D;JEQ");
+
+        // No jump
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("M=0");
+        writer.println("@" + labelEQEnd);
+        writer.println("0;JMP");
+
+        // Jump to here if true/equal
+        writer.println("(" + labelEQTrue + ")");
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("M=-1");
+
+        // End
+        writer.println("(" + labelEQEnd + ")");
+    }
+
+    private void translateCommandGT() {
+        String labelGTTrue = outputName + ".GT.true." + gtCount;
+        String labelGTEnd = outputName + ".GT.end." + gtCount;
+        gtCount++;
+
+        writer.println("// gt");
+
+        // Load second operand
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("D=M");
+        writer.println("@R13");
+        writer.println("M=D");
+
+        // Update stack pointer
+        writer.println("@SP");
+        writer.println("M=M-1");
+
+        // Load first operand
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("D=M");
+
+        // Sub
+        writer.println("@R13");
+        writer.println("D=D-M");
+
+        // Check to jump
+        writer.println("@" + labelGTTrue);
+        writer.println("D;JGT");
+
+        // No jump
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("M=0");
+        writer.println("@" + labelGTEnd);
+        writer.println("0;JMP");
+
+        // Jump to here if true/greater than
+        writer.println("(" + labelGTTrue + ")");
+        writer.println("@SP");
+        writer.println("A=M-1");
+        writer.println("M=-1");
+
+        // End
+        writer.println("(" + labelGTEnd + ")");
     }
 }
